22b7a66c069749b40742a993ab0347cde14a710d
mithileshchellappan <mithilesh153@gmail.com>
2023-10-26 18:11:44 +0530

    Initial Commit

---
+++ cryBto/cryBtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryBtoApp.swift
+//  cryBto
+//
+//  Created by Mithilesh Chellappan on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct cryBtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
